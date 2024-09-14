3573f0902916d9afe3e3a5a13cbfdab00a15af55
Hercalia <minhhoang.harryle@gmail.com>
2024-09-14 11:47:19 +0700

    Update game.py

--- game.py
+++ game.py
@@ -25,6 +25,9 @@ tennis = pygame.image.load("tennis_ball.png")
 run = True
 clock = pygame.time.Clock()
 fps = 60
+screenupt = True
+font1 = pygame.font.Font(None, 80)
+win = 0
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,26 +43,42 @@ while run:
     if keys[pygame.K_DOWN] and racket2.bottom < height:
         racket2.y += racket_speed
     
+    if screenupt:
+        ball.x += ball_speed_x
+        if ball.colliderect(racket1) or ball.colliderect(racket2):
+            ball.x -= ball_speed_x*2
+            ball_speed_x = -ball_speed_x
+        ball.y += ball_speed_y
+        if ball.colliderect(racket1) or ball.colliderect(racket2):
+            ball.y -= ball_speed_y*2
+            ball_speed_y = -ball_speed_y
 
-    ball.x += ball_speed_x
-    if ball.colliderect(racket1) or ball.colliderect(racket2):
-        ball.x -= ball_speed_x*2
-        ball_speed_x = -ball_speed_x
-    ball.y += ball_speed_y
-    if ball.colliderect(racket1) or ball.colliderect(racket2):
-        ball.y -= ball_speed_y*2
-        ball_speed_y = -ball_speed_y
+        if ball.top <= 0 or ball.bottom >= height:
+            ball.y -= ball_speed_y*2
+            ball_speed_y = -ball_speed_y
+            
+        if ball.left <= 0:
+            ball.x -= ball_speed_x*2
+            ball_speed_x = -ball_speed_x
+            screenupt = False
+            screen.blit(font1.render("Player 2 wins", True, (255, 255, 255)), (width // 2 - 200, height // 2 - 50))
+            win = 2
 
-    if ball.top <= 0 or ball.bottom >= height:
-        ball.y -= ball_speed_y*2
-        ball_speed_y = -ball_speed_y
-        
-    if ball.left <= 0 or ball.right >= width:
-        ball.x -= ball_speed_x*2
-        ball_speed_x = -ball_speed_x
-    screen.fill((0, 0, 0))
-    screen.blit(imageracket,(racket1.x,racket1.y))
-    screen.blit(imageracket,(racket2.x,racket2.y))
-    screen.blit(tennis,(ball.x,ball.y))
-    pygame.display.flip()
+        if ball.right >= width:
+            ball.x -= ball_speed_x*2
+            ball_speed_x = -ball_speed_x
+            screenupt = False
+            screen.blit(font1.render("Player 1 wins", True, (255, 255, 255)), (width // 2 - 200, height // 2 - 50))
+            win = 1
+        screen.fill((0, 0, 0))
+        screen.blit(imageracket,(racket1.x,racket1.y))
+        screen.blit(imageracket,(racket2.x,racket2.y))
+        screen.blit(tennis,(ball.x,ball.y))
+        pygame.display.flip()
+    else:
+        if win == 1:
+            screen.blit(font1.render("Player 1 wins", True, (255, 255, 255)), (width // 2 - 200, height // 2 - 50))
+        else:
+            screen.blit(font1.render("Player 2 wins", True, (255, 255, 255)), (width // 2 - 200, height // 2 - 50))
+        pygame.display.flip()
     clock.tick(fps)
